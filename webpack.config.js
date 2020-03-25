208f0317b5f33cb8e940f27d6e8be31a139a2084
Diego Perez <diego@flen.com.ec>
2020-03-24 23:40:43 -0500

    Entorno de trabajo configurado y templates iniciales programados

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path'); //nos permite acceder hacia donde estamos dentro de las carpetas, no importa si esta en desarrollo local o si va a vivir en la nube
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
+            }
+        ])
+    ]
+}
