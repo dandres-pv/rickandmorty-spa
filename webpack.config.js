1875aa2f1f5e589d2db931d718d3977c608fc6dc
Diego Perez <diego@flen.com.ec>
2020-04-06 22:30:16 -0500

    Estilos integrados

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'); //nos permite acceder hacia donde estamos dentro de las carpetas, no importa si esta en desarrollo local o si va a vivir en la nube
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -29,5 +30,9 @@ module.exports = {
         filename: './index.html',
       }
     ),
+    new CopyWebpackPlugin([{
+      from: './src/styles/styles.css',
+      to: ''
+    }])
   ]
 }
